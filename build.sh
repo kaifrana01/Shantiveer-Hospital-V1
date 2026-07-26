8f2bcada9969205372a75f27575dcfc0a75125d5
Abu Hanzala <nadirsheikh768@gmail.com>
2026-07-26 09:19:42 +0530

    fix: force LF line endings on build.sh for Vercel Linux runner

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-pip install --break-system-packages -r requirements.txt
+pip install --break-system-packages -r requirements.txt || pip3 install --break-system-packages -r requirements.txt
 python manage.py collectstatic --noinput
